2219c6efb75ca2fa80e4d7a7d74879ee44b79b7e
Richard Meng <mengangr@foxmail.com>
2023-12-17 10:59:50 +0800

    Homepage feature not work after "npm build"

--- docker/build-ui.sh
+++ docker/build-ui.sh
@@ -2,8 +2,11 @@
 mkdir -p tmp/ui
 cd tmp/ui
 pwd
-git clone https://github.com/Netflix/conductor
+git clone https://github.com/Netflix/conductor -b v3.15.0 --depth=1
 cd conductor/ui
 yarn config set network-timeout 600000 -g
+# Include monaco sources into bundle (instead of using CDN)
+export REACT_APP_MONACO_EDITOR_USING_CDN=false
 yarn install
+cp -r node_modules/monaco-editor public/
 yarn build
